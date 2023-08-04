c3a62abc7c9754eb97e80ff1a27027745c7dec03
Samuel Seo <ngogiaphat0802@gmail.com>
2023-08-04 17:14:00 +0700

    config the package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  pages: true,
+  devtools: {
+    enabled: true,
+  },
+  modules: [
+    "nuxt-icon",
+    "nuxt-lodash",
+    "@pinia/nuxt",
+    '@nuxtjs/supabase',
+    '@nuxtjs/tailwindcss',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
+  runtimeConfig: {
+    public: {
+      stripePk: process.env.STRIPE_PK_KEY
+    }
+  },
+  app: {
+    head: {
+      script: [{
+        src: 'https://js.stripe.com/v3/',
+        defer: true
+      }],
+    }
+  }
+});
